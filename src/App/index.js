356dd0d99fcec75877dfd13ec156f0347b195193
XuyingSwift <xuyingswift@gmail.com>
2021-03-23 16:20:27 -0600

    style-component

--- src/App/index.js
+++ src/App/index.js
@@ -1,17 +1,36 @@
 import React, {Component } from 'react';
 import './App.css';
-import Welcome from './WelcomeMessage'
+import Welcome from './WelcomeMessage';
+import styled, {css} from 'styled-components';
 
+const MyButton = styled.div`
+color:green;
+${props => props.primary && css`
+  color: red;
+`}
+`
+
+const TomatoButton = styled(MyButton)`
+  color:tomato;
+  border-color: black;
+`
 
 class App extends Component {
   render() {
     return (
       // {} javascript expression
-      <Welcome/>
+      <div>
+        <Welcome/>
+        <MyButton>Xuying</MyButton>
+        <MyButton primary>Xuying</MyButton>
+        <TomatoButton>Xuying</TomatoButton>
+
+      </div>
+      
     );
     };
 }
 
 
 
-export default App;
+export default App
