e8a6367e02c50d96d8441e23defb58a84931f3c8
XuyingSwift <xuyingswift@gmail.com>
2021-03-23 16:44:49 -0600

    add bar and gird

--- src/App/index.js
+++ src/App/index.js
@@ -2,13 +2,15 @@ import React, {Component } from 'react';
 import './App.css';
 import Welcome from './WelcomeMessage';
 import AppLayout from './AppLayout'
+import AppBar from './AppBar';
 
 class App extends Component {
   render() {
     return (
       // {} javascript expression
       <AppLayout>
-          <Welcome/>
+        <AppBar/>
+        <Welcome/>
       </AppLayout>
     )
     };
