412720773635d236bdc18c4ed5af699c95f97fe1
XuyingSwift <xuyingswift@gmail.com>
2021-03-23 15:55:22 -0600

    function component

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,16 @@
 import React, {Component } from 'react';
 import './App.css';
 
+// functional component
+function Welcome(props) {
+  return <h1>hello, {props.name}</h1>
+}
+
 class App extends Component {
   render() {
     return (
-      <div>
-          Hello Xuying
-      </div>
+      // {} javascript expression
+      <Welcome name={"CryptoDash"}/>
     );
     };
 }
