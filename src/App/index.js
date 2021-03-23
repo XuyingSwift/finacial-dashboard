fbc407de1df1c82407959f588373dcaddb418fa1
XuyingSwift <xuyingswift@gmail.com>
2021-03-23 16:31:02 -0600

    add app layout

--- src/App/index.js
+++ src/App/index.js
@@ -1,36 +1,17 @@
 import React, {Component } from 'react';
 import './App.css';
 import Welcome from './WelcomeMessage';
-import styled, {css} from 'styled-components';
-
-const MyButton = styled.div`
-color:green;
-${props => props.primary && css`
-  color: red;
-`}
-`
-
-const TomatoButton = styled(MyButton)`
-  color:tomato;
-  border-color: black;
-`
+import AppLayout from './AppLayout'
 
 class App extends Component {
   render() {
     return (
       // {} javascript expression
-      <div>
-        <Welcome/>
-        <MyButton>Xuying</MyButton>
-        <MyButton primary>Xuying</MyButton>
-        <TomatoButton>Xuying</TomatoButton>
-
-      </div>
-      
-    );
+      <AppLayout>
+          <Welcome/>
+      </AppLayout>
+    )
     };
 }
-
-
 
 export default App
