d1bfb527eeebfb23f4cb8f9ce9122380e869652d
XuyingSwift <xuyingswift@gmail.com>
2021-03-25 15:32:36 -0600

    add coin title

--- src/App/Settings/ConfirmButton.js
+++ src/App/Settings/ConfirmButton.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import styled from 'styled-components';
 import {AppContext} from '../AppProvider'
+import {fontSize1, greenBoxShadow, color3} from '../Shared/Styles'
 
 const ConfirmButtonStyled = styled.div`
   margin: 20px;
-  color: darkred;
+  color: ${color3};
   cursor: pointer;
+  ${fontSize1};
+  padding: 5px;
+  &:hover {
+      ${greenBoxShadow}
+  }
 `
 
 const CenterDiv = styled.div`
