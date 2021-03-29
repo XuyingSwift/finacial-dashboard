f3dab5d2b9ad85cdb138127cc3a44a5dfaa83505
XuyingSwift <xuyingswift@gmail.com>
2021-03-29 15:31:25 -0600

    before revert to with price

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -25,32 +25,6 @@ export class AppProvider extends React.Component {
         }
     }
 
-    fetchPrices = async() => {
-        let coinList = (await cc.coinList()).Data;
-        this.setState(coinList)
-
-    }
-
-    fetchPrices = async () => {
-        if(this.state.firstVisit) return;
-        let prices = await this.prices();
-        prices = prices.filter(price => Object.keys(price).length);
-        this.setState({prices});
-      }
-
-      prices = async () => {
-        let returnData = [];
-        for(let i = 0; i < this.state.favorites.length; i++){
-          try {
-            let priceData = await cc.priceFull(this.state.favorites[i], 'USD');
-            returnData.push(priceData);
-          } catch (e){
-            console.warn('Fetch price error: ', e);
-          }
-        }
-        return returnData;
-      }
-    
 
     setFilteredCoins = (filteredCoins) => this.setState({filteredCoins});
 
@@ -70,7 +44,6 @@ export class AppProvider extends React.Component {
     }
     componentDidMount = () => {
         this.fetchCoins();
-        this.fetchPrices();
     }
     fetchCoins = async () => {
         let coinList = (await cc.coinList()).Data;
@@ -83,8 +56,6 @@ export class AppProvider extends React.Component {
             {
                 firstVisit: false,
                 page: 'dashboard'
-            }, () =>  {
-                this.fetchPrices();
             }
         );
         localStorage.setItem('cryptoDash', JSON.stringify(
